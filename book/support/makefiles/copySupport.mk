64e2b56da5583f81ad0b5401b39f3f4d833c9f3c
Damien Pollet <damien.pollet@gmail.com>
2016-09-09 17:51:10 +0200

    Fix support/figures copy commands

--- book/support/makefiles/copySupport.mk
+++ book/support/makefiles/copySupport.mk
@@ -1,4 +1,8 @@
 copySupport: initDir
-	find . -type d -path $(OUTPUTDIRECTORY) -prune -o -wholename "*/figures" -exec cp {} --parents -r ${OUTPUTDIRECTORY} \;
-	cp -r support/ ${OUTPUTDIRECTORY}
-	cp .latexmkrc ${OUTPUTDIRECTORY}
+	find . -type d \
+		-path $(OUTPUTDIRECTORY) -prune \
+		-o -wholename "*/figures" \
+		-exec mkdir -p ${OUTPUTDIRECTORY}/{} \; \
+		-exec rsync -rR {} ${OUTPUTDIRECTORY}/ \;
+	cp -R support ${OUTPUTDIRECTORY}
+	cp latexmkrc ${OUTPUTDIRECTORY}
