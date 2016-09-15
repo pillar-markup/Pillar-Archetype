577dfe5ef1065779897d00ff57baf9a4ad63d4a7
Damien Pollet <damien.pollet@gmail.com>
2016-09-15 17:35:13 +0200

    Auto-download Pillar if needed

--- book/support/makefiles/prepare.mk
+++ book/support/makefiles/prepare.mk
@@ -1,9 +1,9 @@
-.phony: prepare
+.phony: prepare download
 
 $(OUTPUTDIRECTORY):
 	mkdir -p $(OUTPUTDIRECTORY)
 
-prepare: $(OUTPUTDIRECTORY)
+prepare: $(OUTPUTDIRECTORY) pillar mustache
 	git submodule update --init
 	find . -type d \
 		\( -wholename $(OUTPUTDIRECTORY) -or -wholename ./support \) -prune \
@@ -12,3 +12,7 @@ prepare: $(OUTPUTDIRECTORY)
 		-exec mkdir -p $(OUTPUTDIRECTORY)/{} \; \
 		-exec rsync --recursive --relative {}/ $(OUTPUTDIRECTORY) \;
 	ln -fs ../support $(OUTPUTDIRECTORY)/support
+
+pillar mustache: | download
+download:
+	wget --quiet --output-document=- "https://raw.githubusercontent.com/pillar-markup/pillar/master/download.sh" | bash -s $$*
