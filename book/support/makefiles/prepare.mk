f3c6e8d9ad1aaf9109cc7cc337a41322e39b9a6e
Damien Pollet <damien.pollet@gmail.com>
2016-10-04 17:59:02 +0200

    Fix initializing build directory

--- book/support/makefiles/prepare.mk
+++ book/support/makefiles/prepare.mk
@@ -1,8 +1,27 @@
-.phony: prepare download submodules figures
+.phony: prepare buildsupport download submodules
+
+FIGURES := $(shell find . \
+		-type f \
+		-path '*/figures/*' \
+		! -path '$(OUTPUTDIRECTORY)/*' \
+		! -path './support/*' \
+		-print)
 
 # Install build tools & dependencies, create the build directory structure
-prepare: submodules pillar mustache $(OUTPUTDIRECTORY) figures $(OUTPUTDIRECTORY)/support
+prepare: submodules pillar mustache buildsupport
 
+# git doesn't automatically update the contents of submodules
+submodules:
+	git submodule update --init --recursive
+
+# wrapper scripts for pillar and mustache are created together from the pillar install script
+pillar mustache: | download
+download: ## Install Pharo VM & image for Pillar & Mustache
+	wget --quiet --output-document=- "https://raw.githubusercontent.com/pillar-markup/pillar/master/download.sh" | bash -s $$*
+
+# create & initialize output directory, mirroring stuff that has to match the
+# repo hierarchy inside the output dir.
+buildsupport: $(addprefix $(OUTPUTDIRECTORY)/, support gitHeadLocal.gin $(FIGURES))
 
 $(OUTPUTDIRECTORY):
 	mkdir -p $(OUTPUTDIRECTORY)
@@ -10,21 +29,11 @@ $(OUTPUTDIRECTORY):
 $(OUTPUTDIRECTORY)/support: $(OUTPUTDIRECTORY)
 	ln -fs ../support $(OUTPUTDIRECTORY)
 
-# wrapper scripts for pillar and mustache are created together from the pillar install script
-pillar mustache: | download
-download: ## Install Pharo VM & image for Pillar & Mustache
-	wget --quiet --output-document=- "https://raw.githubusercontent.com/pillar-markup/pillar/master/download.sh" | bash -s $$*
+$(OUTPUTDIRECTORY)/gitHeadLocal.gin: $(OUTPUTDIRECTORY) | submodules
+	support/latex/sbabook/gitinfo2.sh > $@
 
-# git doesn't automatically update the contents of submodules
-submodules:
-	git submodule update --init --recursive
-
-# mirror figure directories, since they have to match the repo hierarchy
-# symlinking them would be nice, but rsync is easier
-figures:
-	find . -type d \
-		\( -wholename $(OUTPUTDIRECTORY) -or -wholename ./support \) -prune \
-		-or -name figures -prune \
-		-print \
-		-exec mkdir -p $(OUTPUTDIRECTORY)/{} \; \
-		-exec rsync --recursive --relative {}/ $(OUTPUTDIRECTORY) \;
+# this is making hardlinks (symlinks are verbose with absolute paths and
+# computing relative paths is… complicated)
+$(FIGURES:%=$(OUTPUTDIRECTORY)/%): $(OUTPUTDIRECTORY)/% : %
+	@mkdir -p $(dir $@)
+	ln -f $< $@
